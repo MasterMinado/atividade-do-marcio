1679cf298bc88245ecd4ca69838b70c794ea7b58
gugafreitas621 <gugafreitas621@gmail.com>
2025-05-19 17:33:03 +0000

    Atividade.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 
 function App() {
   const [numero, setNumero] = useState(0);
+  const [num, setnum] = useState(0);
 
   const [mostrar, setMostrar] = useState(false);
 
   return (
     <div>
       <h1>Valor: {numero}</h1>
-      <button onClick={() => setNumero(numero + 100)}>Aumentar +100</button>
+      <button onClick={() => setNumero(numero + num) }>Aumentar +100</button>
 
       <br /><br />
 
