3bb8eca4a676fb2d9aabb9f167eb8a46590a8add
gugafreitas621 <gugafreitas621@gmail.com>
2025-05-19 16:42:40 +0000

    Atividade.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [numero, setNumero] = useState(0);
+
+  const [mostrar, setMostrar] = useState(false);
+
+  return (
+    <div>
+      <h1>Valor: {numero}</h1>
+      <button onClick={() => setNumero(numero + 100)}>Aumentar +100</button>
+
+      <br /><br />
+
+      <button onClick={() => setMostrar(!mostrar)}>
+        {mostrar ? 'Esconder' : 'Mostrar'}
+      </button>
+
+      {mostrar ? <p>parágrafo para esconder e mostrar.</p> : null}
+    </div>
+  );
+}
+
+export default App;
